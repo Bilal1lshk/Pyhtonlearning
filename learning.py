04d77c747669f86dcc873b2a3c24490401bcdfc2
Bilal sheikh <bilalsheikhsb77@gmail.com>
2026-07-05 23:22:33 +0500

    learning

--- learning.py
+++ learning.py
@@ -1,9 +1,25 @@
-print("its working")
-a = 10
-b = 3
-d=2
-print(a)
-print(a+d*b)
-if b > a:   print("its working")
-AGE=int(input("age: "))
-print(AGE)
+#writing a program to print get length
+"""
+print("enter the length")
+length = int(input("Length: "))
+width = int(input("width: "))
+Area=length*width
+print("Area" , Area)
+"""
+#write to print it is even or odd
+"""
+
+print("enter the number")
+
+number = int(input("number: "))
+if number%2==0:
+    print("it is even")
+else:
+    print("it is not even")
+    """
+
+#input two number and get their avg
+a=int(input("a: "))
+b=int(input("b: "))
+avg=int((a + b) / 2)
+print(avg)
