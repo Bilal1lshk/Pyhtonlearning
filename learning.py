cd2c2b61605ac1747de377009c4f7e8752a33c07
Bilal sheikh <bilalsheikhsb77@gmail.com>
2026-07-06 15:53:13 +0500

    working

--- learning.py
+++ learning.py
@@ -19,7 +19,37 @@ else:
     """
 
 #input two number and get their avg
+
+""""
 a=int(input("a: "))
 b=int(input("b: "))
 avg=int((a + b) / 2)
 print(avg)
+"""
+#input two number and greator in it
+"""
+print("ENter number 1")
+
+a=int(input("a: "))
+print("ENter number 2")
+
+b=int(input("b: "))
+
+if a>b:
+    print("a is greator than b")
+elif a<b:
+        print("b is greator than a")
+        """
+str="Python is a powerful and easy-to-learn programming language used for web development, data science, artificial intelligence, automation, game development, and much more. It is loved by beginners and professionals because of its simple syntax, huge community support, and wide range of libraries that make complex tasks much easier"
+print(str.replace("and","or"))
+# print(str.endswith("l"))
+# print(str[-1:-4:-1])
+
+
+#entering name and checking its length
+print("enter your name")
+user=input("name:")
+print(len(user))
+print(user.find('a'))
+
+
