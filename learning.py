a1fcb5ea3d5ad891d4e5ca5fbc78f46eece8b795
Bilal sheikh <bilalsheikhsb77@gmail.com>
2026-07-08 17:32:30 +0500

    done

--- learning.py
+++ learning.py
@@ -54,6 +54,7 @@ print(len(user))
 print(user.find('a'))
 """
 #assigning grade to student according to marks
+"""
 print("if you want to know to grade enter your marks out of hundred")
 marks=1
 marks=int(input("marks:"))
@@ -68,3 +69,36 @@ elif marks>70 and marks<80:
     print("C Grade conngrats") 
 elif marks>70:
     print("D Grade conngrats") 
+    """
+    #making table of any number n
+""" 
+print("give me a number to print table")
+count=1
+number=int(input("number :"))
+while count<=10:
+    print(number,"*",count,"=",count*number)
+    count+=1
+    """ 
+# print("give me a number to print table")
+"""
+count=1
+print(count**count)
+while count<=10:
+    print(count**2)
+    count+=1
+    """
+print("give me a number to find factorial")
+# thistuple=(2,3,4,4,5,5,5)
+# for el in thistuple:
+#     if el==3:
+#         print("finded",el,'at index',thistuple.index(el))
+#     print(el)
+input=int(input("num:"))
+factorial=1
+for i in range(1,input):
+        print("step")
+        factorial *=input  
+        input-=1
+        
+    
+        print("factorial",factorial)
