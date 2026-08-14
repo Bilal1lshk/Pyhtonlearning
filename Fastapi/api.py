aca47230435c208d0d16adf3f57c492591486d76
bilalkamran <bilalsheikhsb77@gmail.com>
2026-08-14 12:10:49 -0700

    http exception time

--- Fastapi/api.py
+++ Fastapi/api.py
@@ -1,5 +1,9 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,HTTPException
 app = FastAPI()
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+@app.get("/getlost")
+def working():
+    raise HTTPException(status_code=201,detail="working perfectly")
+    return {"agayabnharaway"}
