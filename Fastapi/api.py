46b5fdbbd055ab2830230ac344beda3c9dd59779
bilalkamran <bilalsheikhsb77@gmail.com>
2026-08-30 08:21:06 -0700

    making fast apis

--- Fastapi/api.py
+++ Fastapi/api.py
@@ -1,9 +1,27 @@
 from fastapi import FastAPI,HTTPException
-app = FastAPI()
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
-@app.get("/getlost")
-def working():
-    raise HTTPException(status_code=201,detail="working perfectly")
-    return {"agayabnharaway"}
+from pydantic import BaseModel
+from typing import Optional,List
+class Student(BaseModel):
+  name:str
+  age:str
+  married:Optional[List[str]]=None
+  
+def showingnameandage(student:Student):
+  print("working",student.age,student.name)
+student={'name':'bilal','age':'19'}
+student1=Student(**student)
+showingnameandage(student1)
+
+
+
+
+
+
+# app = FastAPI()
+# @app.get("/")
+# def read_root():
+#     return {"Hello": "World"}
+# @app.get("/getlost")
+# def working():
+#     raise HTTPException(status_code=201,detail="working perfectly")
+#     return {"agayabnharaway"}
---
+++ Fastapi/todo.py
@@ -0,0 +1,36 @@
+from typing import Annotated, Literal
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel, Field
+
+app = FastAPI()
+
+class Patient(BaseModel):
+    id: Annotated[int, Field(gt=0)]
+    name: Annotated[str, Field(..., title="Enter the name of the patient")]
+    age: Annotated[int, Field(..., title="Enter the age of the patient")]
+    city: Literal["Delhi", "Mumbai", "Bangalore"]
+
+
+# temporary in-memory "database"
+patients_db = {}
+
+
+@app.get("/createdata")
+def create_model():
+    return {
+        "message": "working",
+        "status": "perfect"
+    }
+
+
+@app.post("/create_patient")
+def create_patient(patient: Patient):
+    if patient.id in patients_db:
+        raise HTTPException(status_code=400, detail="Patient with this ID already exists")
+
+    patients_db[patient.id] = patient.dict()
+
+    return {
+        "message": "Patient created successfully",
+        "data": patients_db[patient.id]
+    }
