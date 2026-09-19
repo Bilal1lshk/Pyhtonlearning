b3c85aa335c3b363bf1329d3cd62ddacb1ab694e
bilalkamran <bilalsheikhsb77@gmail.com>
2026-09-19 20:30:26 +0500

    done

--- Fastapi/rag.py
+++ Fastapi/rag.py
@@ -1,4 +1,21 @@
-from langchain_core.documents import Document
-from langchain_community.document_loaders import TextLoader
-documents = TextLoader("text.txt", encoding="utf-8").load()
-print(documents)
+import warnings
+warnings.filterwarnings("ignore", category=DeprecationWarning)
+
+from langchain_community.document_loaders import (
+    TextLoader,
+    PyMuPDFLoader,
+    DirectoryLoader
+)
+
+loader = DirectoryLoader(
+    path="./data",
+    glob="**/*.pdf",
+    loader_cls=PyMuPDFLoader,
+    show_progress=True
+)
+
+pdf_docs = loader.load()
+print(f"Total PDF pages loaded: {len(pdf_docs)}")
+
+text_docs = TextLoader("text.txt", encoding="utf-8").load()
+print(f"Text file loaded: {len(text_docs)} document(s)")
